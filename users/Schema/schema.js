0c1209358494cb7b854bbf0f3349c0fb5751aa5e
Ryan <ryanwaszak@gmail.com>
2021-06-22 12:39:52 -0500

    resolve circular reference

--- users/Schema/schema.js
+++ users/Schema/schema.js
@@ -1,20 +1,27 @@
 const graphql = require('graphql');
 const axios = require('axios');
-
 const {
   GraphQLObjectType,
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList
 } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString},
     name: { type: GraphQLString},
-    description: { type: GraphQLString}
-  }
+    description: { type: GraphQLString},
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then(res => res.data);
+      }
+    }
+  })
 });
 
 const UserType = new GraphQLObjectType({
@@ -40,6 +47,13 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLString } },
       resolve(parentValue, args) {
         return axios.get(`http://localhost:3000/users/${args.id}`).then(resp => resp.data);
+      }
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`).then(resp => resp.data);
       }
     }
   }
